5a7425e0330f5bcfa0077394b929dbf2cdd9806a
sarath_vasudev <sarathvasudevkarottu@gmail.com>
2024-09-29 15:04:19 +0530

    Add project files.

---
+++ secondGitApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace secondGitApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
